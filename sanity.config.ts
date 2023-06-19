9b9e1b819e1cdc3758c5170af87ffea7d3299ba7
Diana Valaitytė <d.valaityte.97@gmail.com>
2023-06-19 12:10:53 +0300

    Differentiate how env variables are fetched in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,10 +8,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
     name: 'default',
-    title: isProduction ? process.env.SANITY_STUDIO_TITLE : import.meta.env.SANITY_STUDIO_TITLE,
+    title: isProduction ? process.env.NEXT_PUBLIC_SANITY_TITLE : import.meta.env.SANITY_STUDIO_TITLE,
 
-    projectId: isProduction ? process.env.SANITY_STUDIO_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: isProduction ? process.env.SANITY_STUDIO_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
+    projectId: isProduction ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: isProduction ? process.env.NEXT_PUBLIC_SANITY_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
 
     plugins: [deskTool(), visionTool()],
 
