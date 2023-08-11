93ae7f2109f36a14a9fd0d68e0502a2b478c9f0a
Diana Valaitytė <d.valaityte.97@gmail.com>
2023-08-11 20:51:05 +0300

    Implement contact form

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import * as Yup from 'yup';
 import cn from 'clsx';
-import { Formik, Field, Form, FormikHelpers } from 'formik';
+import { Formik, Field, Form } from 'formik';
 
 import styles from './contact.module.scss';
 import { DefaultLayout } from '@/layouts/DefaultLayout/DefaultLayout';
 import { sendEmail } from '@/lib/sendgrid';
-import { AlertComponent, AlertType } from '@/components/Alert/AlertComponent';
-import { IconComponent } from '@/components/Icon/IconComponent';
 
 interface Values {
     from: string;
@@ -15,44 +13,14 @@ interface Values {
 }
 
 const Contact: React.FC = () => {
-    const [alert, setAlert] = React.useState({ message: '', type: '' as AlertType });
-
-    const clearAlert = React.useCallback(() => {
-        setAlert({ message: '', type: 'success' });
-    }, []);
-
     const initialValues = { from: '', text: '' };
     const contactSchema = Yup.object().shape({
         from: Yup.string().email('Invalid email').required('Required'),
         text: Yup.string().min(2, 'Too Short!').max(900, 'Too Long!').required('Required'),
     });
 
-    const handleSubmit = React.useCallback(async ({ from, text }: Values, { resetForm }: FormikHelpers<Values>) => {
-        await sendEmail({ from, text })
-            .then(() => {
-                setAlert({
-                    message: 'Your enquiry has been successfully sent',
-                    type: AlertType.Success,
-                });
-                setTimeout(() => {
-                    setAlert({ message: '', type: '' });
-                }, 5000);
-
-                resetForm();
-
-                return;
-            })
-            .catch((error) => {
-                setTimeout(() => {
-                    setAlert({
-                        message: 'An error has occurred, please try again later',
-                        type: AlertType.Error,
-                    });
-                }, 5000);
-                setAlert({ message: '', type: '' });
-
-                return;
-            });
+    const handleSubmit = React.useCallback(async ({ from, text }: Values) => {
+        await sendEmail({ from, text });
     }, []);
 
     return (
@@ -98,17 +66,6 @@ const Contact: React.FC = () => {
                             <button className={styles.submitButton} type="submit" disabled={isSubmitting || !dirty}>
                                 Submit
                             </button>
-                            {alert.message && (
-                                <AlertComponent
-                                    message={alert.message}
-                                    color={alert.type}
-                                    header={alert.type}
-                                    leadingIcon={<IconComponent name={alert.type} />}
-                                    trailingIcon={
-                                        <IconComponent name="cross" className="text-grey-500" onClick={clearAlert} />
-                                    }
-                                />
-                            )}
                         </Form>
                     )}
                 </Formik>
