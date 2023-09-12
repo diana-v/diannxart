3bc626988da6f39a75ba2d84ff879ae0c8d675bc
diana-v <d.valaityte.97@gmail.com>
2023-09-12 16:06:14 +0300

    Increase static page generation timeout

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withPWA = require("next-pwa")({
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
     reactStrictMode: true,
+    staticPageGenerationTimeout: 100,
     images: {
         domains: ['cdn.sanity.io']
     },
