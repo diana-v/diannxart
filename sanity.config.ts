0e051f5119ec53d39bc3c2221149746857b05048
diana-v <d.valaityte.97@gmail.com>
2023-11-29 16:32:31 +0200

    Enable ability to sort posts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
+import { orderableDocumentListDeskItem } from '@sanity/orderable-document-list';
 
 import { schemaTypes } from './src/studio/schemas';
 
@@ -13,7 +14,20 @@ export default defineConfig({
     projectId: isProduction ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
     dataset: isProduction ? process.env.NEXT_PUBLIC_SANITY_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
 
-    plugins: [deskTool(), visionTool()],
+    plugins: [
+        deskTool({
+            structure: (S, context) => {
+                return S.list()
+                    .title('Content')
+                    .items([
+                        orderableDocumentListDeskItem({ type: 'post', title: 'Post', S, context }),
+                        S.documentTypeListItem('author'),
+                        S.documentTypeListItem('category'),
+                    ]);
+            },
+        }),
+        visionTool(),
+    ],
 
     schema: {
         types: schemaTypes,
