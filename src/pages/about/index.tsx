c61dab7a20865d7926826ae398aecc3c004f910a
Diana Valaityte <d.valaityte.97@gmail.com>
2026-03-18 13:47:47 +0200

    Update tailwind

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -10,7 +10,7 @@ const About = () => {
 
     return (
         <DefaultLayout description={localisedString.about.seoDescription} title={localisedString.about.seoTitle}>
-            <div className="container flex flex-col mx-auto px-4 py-3 gap-8 flex-grow max-w-5xl">
+            <div className="container flex flex-col mx-auto px-4 py-3 gap-8 grow max-w-5xl">
                 <h1 className="uppercase text-5xl md:text-7xl font-serif font-thin text-center transition-[font-size] ease-in duration-500">{localisedString.about.title}</h1>
                 <article className="flex flex-col gap-4 text-xl md:text-2xl">
                     <p>{localisedString.about.description.first}</p>
